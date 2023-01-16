4d8022a3f041c331b45176e132fea24806a958b0
Accelerator87 <n126970@gmail.com>
2023-01-17 03:46:15 +0800

    Add lights generation script & modify the brightness

--- Assets/Scripts/GenerationManager.cs
+++ Assets/Scripts/GenerationManager.cs
@@ -1,24 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public enum GenerationState {
+    Idle,
+    GeneratingRooms,
+    GeneratingLights
+}
+
 public class GenerationManager : MonoBehaviour
 {
     // parent of the world
     [SerializeField] private Transform WorldGrid;
     
-    // prefab of the rooms
+    // prefabs of the rooms
     [SerializeField] private List<GameObject> RoomTypes;
     [SerializeField] private GameObject EmptyRoom;
     [SerializeField] private int GenEmptyChance;
     [SerializeField] private Slider EmptinessSlider;
+
+    // prefabs of the lights
+    [SerializeField] private List<GameObject> LightTypes;
+    [SerializeField] private int GenLightChance;
+    [SerializeField] private Slider BrightnessSlider;
     
     // size of the map and room
     [SerializeField] private int MapSize = 16;
     [SerializeField] private float RoomSize = 7;
 
+    // settings when generating world
     [SerializeField] private Slider MapSizeSlider;
     [SerializeField] private Button GenerateBotton;
 
@@ -26,10 +39,12 @@ public class GenerationManager : MonoBehaviour
     private float nowX = 0, nowY = 0, nowZ = 0;
     private Vector3 nowPos;
     private int nowPosTracker = 0;
+    private GenerationState nowState;
 
     private void Update() {
         MapSize = (int)Mathf.Pow(MapSizeSlider.value, 4);
         GenEmptyChance = (int)EmptinessSlider.value;
+        GenLightChance = (int)BrightnessSlider.maxValue - (int)BrightnessSlider.value;
         MapSizeRoot = (int)Mathf.Sqrt(MapSize);
     }
 
@@ -37,28 +52,58 @@ public class GenerationManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    private void GoNextState() {
+        nowState ++;
+        nowX = 0; nowY = 0; nowZ = 0;
+        nowPosTracker = 0;
+        nowPos = Vector3.zero;
+    }
+
     public void GenerateWorld() {
 
         GenerateBotton.interactable = false;
 
-        for (int i = 0; i < GenEmptyChance; i++) {
-            RoomTypes.Add(EmptyRoom);
-        }
+        int AllState = Enum.GetNames(typeof(GenerationState)).Length;
+        for (int StateID = 0; StateID < AllState; StateID++) {
 
-        for (int i = 0; i < MapSize; i++) {
-
-            if(nowPosTracker == MapSizeRoot) {
-                nowPosTracker = 0;
-                nowX = 0;
-                nowZ += RoomSize;
+            for (int i = 0; i < GenEmptyChance; i++) {
+                RoomTypes.Add(EmptyRoom);
             }
 
-            nowPos = new(nowX, nowY, nowZ);
-            Instantiate(RoomTypes[Random.Range(0, RoomTypes.Count)], nowPos, Quaternion.identity, WorldGrid);
+            for (int i = 0; i < MapSize; i++) {
 
-            nowPosTracker ++;
-            nowX += RoomSize;
+                if(nowPosTracker == MapSizeRoot) {
+                    nowPosTracker = 0;
+                    nowX = 0;
+                    nowZ += RoomSize;
+                }
+                nowPos = new(nowX, nowY, nowZ);
+
+                switch (nowState) {
+                    case GenerationState.GeneratingRooms: {
+                        Instantiate(RoomTypes[UnityEngine.Random.Range(0, RoomTypes.Count)]
+                                    , nowPos, Quaternion.identity, WorldGrid);
+                        break;
+                    }
+
+                    case GenerationState.GeneratingLights: {
+                        System.Random rnd = new System.Random();
+                        int rand = rnd.Next(0, GenLightChance + 1);
+                        if(rand <= 1) {
+                            Instantiate(LightTypes[UnityEngine.Random.Range(0, LightTypes.Count)]
+                                       , nowPos, Quaternion.identity, WorldGrid);
+                        }
+                        break;
+                    }
+                }
+                
+                nowPosTracker ++;
+                nowX += RoomSize;
+            }
+
+            GoNextState();
         }
     }
+
 
 }

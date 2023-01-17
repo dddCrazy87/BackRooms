01f37a89e48f0c0eb25276ff0abd793ea13f9ca1
Accelerator87 <n126970@gmail.com>
2023-01-18 03:23:44 +0800

    Add the script that happens when the player touch the exit

---
+++ Assets/Scripts/Exit.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    [SerializeField] private GameObject WinningMsg;
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") {
+            WinningMsg.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+}
